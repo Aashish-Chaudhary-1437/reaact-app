13b654107bb701a0e5ae94417b90cfd8fb1e915c
Aashish <aachita1437@gmail.com>
2026-06-07 23:26:32 +0545

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar"
+
+const App = () => {
+
+  return (
+    <div>
+      <h1>Hello, I am react</h1>
+     <Navbar />
+     <Navbar />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+
+const Navbar = () => {
+  return (
+    <div>
+        <div className="box">
+        <h1>Hi, I am navbar</h1>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Navbar
+
